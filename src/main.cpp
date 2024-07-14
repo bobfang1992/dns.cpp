f806e02e134e098d4760ccf79f235671a843a9fb
Bob Fang <bob.fang.london@gmail.com>
2024-07-14 17:55:00 -0400

    try out libuv

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,41 @@
-#include "resolver.h"
+#include <uvw.hpp>
 #include <iostream>
+#include <memory>
+#include <string>
+#include <cstring>
 
-int main() {
-  auto ip = dnscpp::Resolver::resolve("google.com");
-  std::cout << "result = " << dnscpp::to_string(ip) << std::endl;
+void on_resolved(std::shared_ptr<uvw::Loop> loop, const std::string &domain) {
+  auto resolver = loop->resource<uvw::GetAddrInfoReq>();
+
+  resolver->on<uvw::ErrorEvent>([](const uvw::ErrorEvent &event, uvw::GetAddrInfoReq &) {
+    std::cerr << "Error: " << event.what() << std::endl;
+  });
+
+  resolver->on<uvw::AddrInfoEvent>([](const uvw::AddrInfoEvent &event, uvw::GetAddrInfoReq &req) {
+    char addr[17] = {'\0'};
+    uv_ip4_name(reinterpret_cast<struct sockaddr_in *>(event.data->ai_addr), addr, 16);
+    std::cout << "Resolved IP: " << addr << std::endl;
+  });
+
+  addrinfo hints;
+  std::memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_STREAM;
+
+  resolver->nodeAddrInfo(domain, &hints);
+}
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    std::cerr << "Usage: " << argv[0] << " [domain]" << std::endl;
+    return 1;
+  }
+
+  std::string domain = argv[1];
+
+  auto loop = uvw::Loop::getDefault();
+  on_resolved(loop, domain);
+  loop->run();
+
   return 0;
 }
--- xmake.lua
+++ xmake.lua
@@ -2,9 +2,14 @@ add_rules("mode.debug", "mode.release")
 set_languages("c++20")
 
 add_requires("sqlite3")
+add_requires("libuv")
+add_requires("uvw")
 
-target("dns.cpp")
+target("dns")
     set_kind("binary")
     add_includedirs("include")
     add_files("src/*.cpp")
     add_packages("sqlite3")
+    add_packages("libuv")
+    add_packages("uvw")
+
